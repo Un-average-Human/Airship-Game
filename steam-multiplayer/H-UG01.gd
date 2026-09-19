58b10c1ecdcb5c174e6b6657e8a946d6ef59b9e1
Un-average-Human <miguelmfernandes1008@gmail.com>
2026-09-19 14:02:52 +1200

    plane update
    SUSPENSION WORKKSS!!! alongside the pitch which now uses physics and doesnt slide thanks to a good friend and his physics knowledge.

--- steam-multiplayer/H-UG01.gd
+++ steam-multiplayer/H-UG01.gd
@@ -8,12 +8,13 @@ extends RigidBody3D
 @export var max_accel: float = 150
 
 @export_subgroup("Pitch")
-@export var pitch_torque: float = 150
+@export var lateral_drag: float = 5000.0
+@export var vertical_drag: float = 5000.0
+@export var pitch_torque: float = 2500
 
 var acceleration: float = 0.0
 var throttle: float = 0.0
 var target_speed: float = 0.0
-var current_speed: float = 0.0
 
 var throttle_input
 var pitch_input
@@ -23,12 +24,18 @@ func _physics_process(delta: float) -> void:
 	throttle_input = Input.get_axis("throttle_down", "throttle_up")
 	if throttle_input != 0.0:
 		throttle = clampf(throttle + (throttle_increase * throttle_input), 0.0, 1.0)
-		print("Throttle: ", throttle * 100, "%")
-		print("Target Speed: ", target_speed, "m/s")
-		print("Velocity is: ", linear_velocity.z)
 	
 	target_speed = remap(throttle, 0.0, 1.0, 0.0, max_speed)
-	physics_material_override.friction = remap(throttle, 0.0, 1.0, 1.0, 0.0)
+	
+	#DRAG
+	var local_vel: Vector3 = global_transform.basis.inverse() * linear_velocity
+	
+	#multiply by throttle so itll work with the gravity yk?
+	var lateral_drag_force: Vector3 = -global_transform.basis.x * (local_vel.x * lateral_drag * throttle)
+	var vertical_drag_force: Vector3 = -global_transform.basis.y * (local_vel.y * vertical_drag * throttle)
+	
+	apply_central_force(lateral_drag_force)
+	apply_central_force(vertical_drag_force)
 	
 	#ACCELERATION
 	var target_max_accel = remap(throttle, 0.0, 1.0, 0.0, max_accel)
@@ -36,14 +43,14 @@ func _physics_process(delta: float) -> void:
 	if throttle_input == 0.0:
 		acceleration = move_toward(acceleration, 0.0, accel_rate * delta)
 	
-	apply_central_force(Vector3.FORWARD * target_speed * acceleration)
-	linear_velocity = linear_velocity.limit_length(max_speed)
+	apply_central_force(-global_transform.basis.z * target_speed * acceleration)
 	
+	if linear_velocity.length() > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
 	
 	##GRAVITY
 	gravity_scale = remap(throttle, 0.0, 1.0, 1.0, 0.0)
 	
-	
 	##PITCH
 	pitch_input = Input.get_axis("pitch_down", "pitch_up")
-	
+	apply_torque(global_transform.basis.x * pitch_input * pitch_torque)
